26676e5f0f359ebb0d0e4fa9fa246b67dffcb3ed
huangzhangting <huangzhangting@wdai.com>
2019-09-09 10:09:28 +0800

    gc

--- athena-test/src/main/java/org/tin/athena/test/StringTest.java
+++ athena-test/src/main/java/org/tin/athena/test/StringTest.java
@@ -6,6 +6,8 @@ public class StringTest {
         String s1 = new StringBuffer("计算机").append("转件").toString();
         System.out.println(s1.intern() == s1);
 
+        //1.7版本以后，false，因为java特殊字符串，常量池中已经有了
+        //不符合首次出现的原则
         String s2 = new StringBuffer("ja").append("va").toString();
         System.out.println(s2.intern() == s2);
     }
