fb535e1515327ac7af0f507554607afeed7510c6
huangzhangting <huangzhangting@wdai.com>
2019-11-19 10:18:19 +0800

    数据库

---
+++ athena-test/src/main/java/org/tin/athena/test/OverloadTest.java
@@ -0,0 +1,23 @@
+package org.tin.athena.test;
+
+import java.util.List;
+
+public class OverloadTest {
+
+    public static int me(List<Integer> list){
+        System.out.println("m1111");
+        return 0;
+    }
+
+//    public static String me(List<String> list){
+//        System.out.println("m222");
+//        return "";
+//    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
